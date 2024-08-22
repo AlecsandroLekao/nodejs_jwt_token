b052744d93c14330bf1355638ffbb1b79e7f56ef
Alecsandro <lekao2009@gmail.com>
2024-08-21 23:27:38 -0300

    start

--- src/Server.js
+++ src/Server.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import publicRoutes from './routes/public.js'
+import publicRoutes from './routes/public.js';
 
 const app = express();
-app.use(express.json())
-app.use('/', publicRoutes)
+app.use(express.json());
+app.use('/', publicRoutes);
 
-app.get('/',(req,res)=>{
-    return res.json({message:'alexxxx'})
-})
+app.get('/', (req, res) => {
+  return res.json({ message: 'alexxxx' });
+});
 
-app.listen(3000, ()=> console.log('oi'))
+app.listen(3000, () => console.log('oi'));
--- src/routes/public.js
+++ src/routes/public.js
@@ -1,55 +1,51 @@
 import express from 'express';
-import { Prisma, PrismaClient } from '@prisma/client'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import { Prisma, PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express.Router();
 const prisma = new PrismaClient();
 const JWT_SECRET = process.env.JWT_SECRET;
 
+app.post('/cadastros', async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
 
-app.post('/cadastros', async (req,res)=>{
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-    try {
+    const userdb = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hash,
+      },
+    });
+    return res.status(200).json(userdb);
+  } catch (error) {
+    return res.status(500).json('ocoreu um erro ' + error);
+  }
+});
 
-        const {email,name,password} = req.body;
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt)
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
 
-       const userdb = await prisma.user.create({
-            data:{
-            email,name,
-            password: hash
-            }
-        })
-        return res.status(200).json(userdb);
-    } catch (error) {
-        return res.status(500).json('ocoreu um erro '+ error);
-    }
+  if (!user) {
+    return res.status(404).json({ message: 'ocorreu um erro' });
+  }
 
-})
+  const isMatch = await bcrypt.compare(password, user.password);
 
-app.post('/login', async (req,res)=>{
-    const {email, password} = req.body;
+  if (!isMatch) {
+    return res.status(403).json({ message: 'senha invalida' });
+  }
 
-   const user = await prisma.user.findUnique({
-        where : {email}
-    })
+  const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1m' });
 
-    if (!user){ 
-        return res.status(404).json({message:'ocorreu um erro'}); 
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password )
-
-    if (!isMatch) {
-        return res.status(403).json({message:'senha invalida'});
-    }
-
-
-    const token = jwt.sign({id:user.id}, JWT_SECRET,{expiresIn:'1m'})
-
-    return res.status(200).json(token);
-})
+  return res.status(200).json(token);
+});
 export default app;
